5e3585a45d45bfffecbd79e2fe994efea56b3705
J76F <105970388+J76F@users.noreply.github.com>
2022-06-15 19:44:31 +0200

    SQL output variabelen

--- config.php
+++ config.php
@@ -7,6 +7,15 @@ $song_db_path = __DIR__ . '/databases/Songs.db';
 $song_words_db_path = __DIR__ . '/databases/SongWords.db';
 $output_directory = __DIR__ . '/output/';
 
+//sql output setting
+$sql_settings = array(
+	'database' => "database",
+	'server' => "localhost", //"localhost:3306",
+	'gebruikersnaam' => "username",
+	'wachtwoord' => "wachtwoord",
+	'tabel' => "songs",	//	ID,	Titel, Auteur, Copyright, ccli_no, Lyrics, Date
+);
+
 // Custom Settings
 $custom_settings = array(
     'capitalize_names' => false, // Capitalize some property names, defined in $words_to_capitalize
@@ -18,7 +27,7 @@ $custom_settings = array(
     'standardize_song_sections' => false, // Standardize the names of the song sections to fit ProPresenter's defaults
     'standardize_title_format' => false, // Standardize the formatting of the name of the songs
     'prevent_overwrites' => true, // Prevent overwriting files - adds a '(1)' style suffix
-    'add_metadata_to_export_files' => true, // Adds the metadata block to the top of the export files
+    'add_metadata_to_export_files' => false, // Adds the metadata block to the top of the export files
     'condense_slide_breaks' => false, // Condense all slide breaks
     'reflow_large_blocks' => false, // Reflow large blocks (target line length defined in $reflow_max_lines)
     'output_subdirectory' => true, // Output files in a timestamp labelled sub-directory
@@ -29,7 +38,7 @@ $custom_settings = array(
 );
 
 $reflow_max_lines = 2; // How many lines should we try to 'reflow' the text to?
-$file_export_type = 'plain_text'; // set to 'propresenter6' to use the experimental ProPresenter6 output
+$file_export_type = 'sql'; // set 'sql', 'plain_text', set to 'propresenter6' to use the experimental ProPresenter6 output
 
 $song_section_names = array('Verse', 'Chorus', 'Pre-Chorus', 'Bridge', 'Tag', 'Intro', 'End');
 $words_to_capitalize = array('Jesus', 'God', 'Gud', 'Lord', 'You', 'Your', 'Du', 'Din', 'Ditt', 'Han', 'Hans', 'Ham', 'Holy Spirit', 'Father');
